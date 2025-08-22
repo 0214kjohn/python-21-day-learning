09487ab62c04e2aaaa96a951995a682c93eb59a8
0214kjohn <arintasai.k@gmail.com>
2025-08-22 22:24:25 +0700

    Day 2: Completed Hello World challenge

---
+++ day_01.py
@@ -0,0 +1,7 @@
+print("My name is John Arintasai")
+print("I am 20 years old")
+print("My goal is to become a software engineer")
+print("I am currently learning Python")
+print("I enjoy solving problems and building applications")
+
+
---
+++ day_02.py
@@ -0,0 +1,9 @@
+first_name = "John"
+last_name = "Arintasai"
+age = 20
+height_meters = 1.65
+
+print(f"My name is {first_name} - {last_name}. I am {age} years old and I am {height_meters} meters tall.")
+print(f"Type of age is {type(age)} and type of height is {type(height_meters)}.")
+
+
